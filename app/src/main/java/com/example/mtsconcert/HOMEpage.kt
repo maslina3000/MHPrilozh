13d7aaa6634f9b94d905621e8831b58c8350c03e
maslina3000 <andrey.worobyov46@gmail.com>
2019-03-23 14:56:09 +0300

    дезайн

---
+++ app/src/main/java/com/example/mtsconcert/HOMEpage.kt
@@ -0,0 +1,15 @@
+package com.example.mtsconcert
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class HOMEpage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_homepage)
+    }
+
+}
--- app/src/main/java/com/example/mtsconcert/MainActivity.kt
+++ app/src/main/java/com/example/mtsconcert/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.mtsconcert
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun login(view: View)
+    {
+        val intent = Intent(this, HOMEpage::class.java).apply {
+
+            //putExtra(AlarmClock.EXTRA_MESSAGE, "")
+
+        }
+
+        startActivityForResult(intent, 254)
     }
 }
